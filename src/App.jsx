5fde57329bdc2f2e865aac459e8d4afb03b218eb
Uday Shastrakar <uday.shastrakar@gmail.com>
2025-06-26 19:55:28 +0530

    base url changes added

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,9 @@
-// src/App.jsx
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Header from "./components/Header";
-import Footer from "./components/Footer";
-import Banner from "./components/Banner";
-import About from "./components/About";
-import Services from "./components/Services";
-import Expertise from "./components/Expertise";
-import AboutPage from "./pages/AboutPage";
-import Contact from "./pages/Contact";
-import PrivacyPolicy from "./pages/PrivacyPolicy";
-import Licensing from "./pages/Licensing";
-import Skills from "./pages/Skills";
-import Portfolio from "./pages/Portfolio";
-
+// ... your imports
 
 function App() {
   return (
-    <Router>
+    <Router basename="/PortfolioWebsite"> {/* Add basename here */}
       <Header />
       <Routes>
         <Route
@@ -27,19 +14,16 @@ function App() {
               <About />
               <Services />
               <Expertise />
-        
-              
             </>
           }
         />
         <Route path="/about" element={<AboutPage />} />
         <Route path="/contact" element={<Contact />} />
         <Route path="/privacy" element={<PrivacyPolicy />} />
-        <Route path="/licensing" element={<Licensing/>} />
-        <Route path="/skills" element={<Skills />}/>
-        <Route path="/portfolio" element={<Portfolio/>} />
+        <Route path="/licensing" element={<Licensing />} />
+        <Route path="/skills" element={<Skills />} />
+        <Route path="/portfolio" element={<Portfolio />} />
       </Routes>
-      
       <Footer />
     </Router>
   );
